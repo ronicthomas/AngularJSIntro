3e8da9ae68bb917953002e829a71941f45c0c089
Roni C Thomas <roni@intelligrape.com>
2015-03-28 11:33:36 +0530

    Added bootstrap data

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,11 @@
+import com.intelligrape.intellimeet.Todo
+
 class BootStrap {
 
     def init = { servletContext ->
+        ["Eat", "Drink", "Be merry", "Scare the world", "Sleep"].each {
+            Todo.findOrSaveWhere(task: it)
+        }
     }
     def destroy = {
     }
